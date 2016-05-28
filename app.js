1d268543185f8e62504e6fbac017055e2ceee01c
xinqing520 <1161751421@qq.com>
2016-05-29 00:35:15 +0800

    添加了package.json

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 
 var http = require("http");
 http.createServer(function (req,res) {
-    console.log(req.pathname);
+    res.write("这是我的第一个网站");
+    res.end();
 }).listen(3001, function () {
     console.log(`3001 is listening`);
 
